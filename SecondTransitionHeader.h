99e53c63a6781cbaf9ef332a76c074f5a19b1dcb
riron22 <riron22@walla.co.il>
2017-08-04 21:29:34 +0300

    Update SecondTransitionHeader.h

--- SecondTransitionHeader.h
+++ SecondTransitionHeader.h
@@ -5,6 +5,8 @@ typedef struct parms
 	registers* regDestination;
 	eCpuRegisters eRegSource;
 	eCpuRegisters eRegDestination;
+	pSymbol symbolSource;
+	pSymbol symbolDestination;
 	short sourceNum;
 } parms;
 
@@ -14,24 +16,86 @@ char *name;
 void (*func)(parms);
 } cmd;
 
+void readdressSymbolTable();
+
+/*mov commands*/
 void movTwoRegisterCmd(parms data);
-void movRegisterValueCmd(parms data);
+void movRegisterSymbolCmd(parms data);
+void movSymbolRegisterCmd(parms data);
+void movSymbolSymbolCmd(parms data);
+void movValueRegisterCmd(parms data);
+void movValueSymbolCmd(parms data);
+
+/*cmp commands*/
 void cmpTwoRegisterCmd(parms data);
-void cmpRegisterValueCmd(parms data);
+void cmpRegisterSymbolCmd(parms data);
+void cmpSymbolRegisterCmd(parms data);
+void cmpSymbolSymbolCmd(parms data);
+void cmpValueRegisterCmd(parms data);
+void cmpValueSymbolCmd(parms data);
+
+/*add commands*/
 void addTwoRegisterCmd(parms data);
-void addRegisterValueCmd(parms data);
+void addRegisterSymbolCmd(parms data);
+void addSymbolRegisterCmd(parms data);
+void addSymbolSymbolCmd(parms data);
+void addValueRegisterCmd(parms data);
+void addValueSymbolCmd(parms data);
+
+/*sub commands*/
 void subTwoRegisterCmd(parms data);
-void subRegisterValueCmd(parms data);
+void subRegisterSymbolCmd(parms data);
+void subSymbolRegisterCmd(parms data);
+void subSymbolSymbolCmd(parms data);
+void subValueRegisterCmd(parms data);
+void subValueSymbolCmd(parms data);
+
+/*lea commands*/
 void leaTwoRegisterCmd(parms data);
-void leaRegisterValueCmd(parms data);
+void leaRegisterSymbolCmd(parms data);
+void leaSymbolRegisterCmd(parms data);
+void leaSymbolSymbolCmd(parms data);
+void leaValueRegisterCmd(parms data);
+void leaValueSymbolCmd(parms data);
+
+/*not commands*/
 void notRegisterCmd(parms data);
+void notSymbolCmd(parms data);
+
+/*clr commands*/
 void clrRegisterCmd(parms data);
+void clrSymbolCmd(parms data);
+
+/*inc commands*/
 void incRegisterCmd(parms data);
+void incSymbolCmd(parms data);
+
+/*dec commands*/
 void decRegisterCmd(parms data);
+void decSymbolCmd(parms data);
+
+/*jmp commands*/
 void jmpRegisterCmd(parms data);
+void jmpSymbolCmd(parms data);
+
+/*bne commands*/
 void bneRegisterCmd(parms data);
+void bneSymbolCmd(parms data);
+
+/*red commands*/
 void redRegisterCmd(parms data);
+void redSymbolCmd(parms data);
+
+/*prn commands*/
 void prnRegisterCmd(parms data);
+void prnSymbolCmd(parms data);
+
+/*jsr commands*/
 void jsrRegisterCmd(parms data);
+void jsrSymbolCmd(parms data);
+
+/*rts command*/
 void rtsCmd(parms data);
+
+/*stop command*/
 void StopCmd(parms data);
