4e566cd31b157a1d05d9c328c20497a5fdb69e1c
user <user@ubuntu.(none)>
2017-08-12 09:56:33 -0400

    Corrected assembler

--- assembler.c
+++ assembler.c
@@ -1,60 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "SharedHeaderFile.h"
-#include "FirstTransitionHeader.h"
+#include "assembler.h"
 
-opcodeStruct opcodesArray[16]={
-	{0 , "mov"},
-	{1 , "cmp"},
-	{2 , "add"},
-	{3 , "sub"},
-	{4 , "not"},
-	{5 , "clr"},
-	{6 , "lea"},
-	{7 , "inc"},
-	{8 , "dec"},
-	{9 , "jmp"},
-	{10 , "bne"},
-	{11 , "red"},
-	{12 , "prn"},
-	{13 , "jsr"},
-	{14 , "rts"},
-	{15 , "stop"},
-};
-Errorptr errorList = NULL;
-Errorptr errorListLast = NULL;	
-
-int main( int argc, char *argv[] ){
-	int i;
-	char *token;
-	int DC = 0; 
-	int IC = 100;
-	
-	FILE **files; /* a pointer to the files given as input from command-line*/ 
-	errorType error; /* used for errorType enum*/ 
-	
+void validateFileName(char *token);
+FILE *handleFileName(char *filename);
+int main(int argc, char *argv[]){
+	FILE *fd;	
+	char *suffix;
+	char *fileName;
+	int argumentIndex = 1;
 	/*First, check for command-line argument error. If An error is found, 			program stops immediately*/	
-	printf("%d\n", argc);	
-	printf("line 19\n");
 	if (argc<2){
-		printf("line 21\n");
-		error = WRONG_COMMAND_LINE_INPUT;
-		addError(&errorList, &errorListLast, error, 0);
-		printErrorList(errorList);
+		printf("No File Reference in Command-Line Input\n");
 		exit(0);
 	}
-	for (i=1;i<argc;i++){
-		token = strtok(argv[i],".");
-		token = strtok(NULL ,".");
-		if (strcmp(token,"as") != 0){
-			error = WRONG_COMMAND_LINE_INPUT;
-			addError(&errorList, &errorListLast, error, 0);
-			printErrorList(errorList);
+	printf("line 19\n");
+	while (argumentIndex<argc){
+		printf("%d\n", argumentIndex);		
+		fileName = (char*) malloc(sizeof(char)*maxFileName);
+		if (!fileName){
+			fprintf(stderr, "Memory allocation for file name failed\n");
 			exit(0);
-		}	
+		}
+		strcpy(fileName,argv[argumentIndex]);	
+		suffix = strtok(argv[argumentIndex],".");
+		suffix = strtok(NULL,".");
+		validateFileName(suffix);		
+		fd = handleFileName(fileName);
+		/*fileFirstTransition(fd);*/
+		printf("Let's Start!\n"); /*just for debugging*/
+		argumentIndex++;
 	}
-	printf("Let's Start!\n"); /*just for debugging*/
+	
 	return 0;	
 }
 
+void validateFileName(char *token){
+	if ((token == NULL) || (strcmp(token,inputFileSuffix)!=0)){
+		fprintf(stderr, "Wrong File Type as Argument\n");
+		exit(0);
+	}
+}
+
+FILE *handleFileName(char *filename){
+	FILE *f;	
+	if (!(f = fopen(filename,"r")))
+	{
+		fprintf(stderr,"cannot open file\n");
+		exit(0);
+	} else {
+		return f;
+	}
+}
---
+++ assembler.h
@@ -0,0 +1,7 @@
+#define inputFileSuffix "as"
+#define maxFileName 30
+
+typedef struct nameFile{
+	char *name;
+	char *suffix;
+}nameFile;
