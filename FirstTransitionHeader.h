63773d4d4810b9d7b1203ecbf1eecdbe4f25c015
riron22 <riron22@walla.co.il>
2017-08-19 19:17:44 +0300

    Update FirstTransitionHeader.h

--- FirstTransitionHeader.h
+++ FirstTransitionHeader.h
@@ -1,8 +1,12 @@
 
+
 extern char *instructions[];
 extern char *registerNames[];
 extern int flagLabel;
 typedef struct binWord{
 	short a : 10;
 }binWord;
+
+void analizeLine(char *line, int lineNum);
+void moveOverFileOne(FILE* currentFile);
 
