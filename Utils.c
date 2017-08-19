220cda3d6b114e34fd8d630be0f0af404942e19f
riron22 <riron22@walla.co.il>
2017-08-19 14:18:44 +0300

    Update Utils.c

--- Utils.c
+++ Utils.c
@@ -1,230 +1,150 @@
 #include "SharedHeaderFile.h"
 
-
-
 /*A method to convert a short number to string of letters in the wierd four counting system*/
-
-
-
 char* convertToWeirdFour(short wordShort)
-
 {
-
 	int i , wordLoc = 0;
-
-	short num;
-
+	int num;
 	char* weirdFourWord;
 
 	weirdFourWord = (char*)malloc(sizeof(char)*6);
-
 	if(!weirdFourWord)
-
 		return NULL;
-
-	for(i = 0; i < WORDLENGTH; i += 2)
-
+	wordLoc = (WORDLENGTH/2) - 1;
+	for(i = 0; i < WORDLENGTH ; i += 2)
 	{
-
 		/*Calculate the number from two adjacent bit*/
-
-
-
-		num = (wordShort & (1  << i)) + 2 * (wordShort & (1  << (i+1)));
-
+		num = ((wordShort & (1  << i))>>i) + 2 * ((wordShort & (1  << (i+1)))>>(i+1));
 		/*Fit the right number to the number according to the wierdFourCountNums enum*/
-
-
-
 		switch(num)
-
 		{
 
 			case A : weirdFourWord[wordLoc] = 'a'; break;
-
 			case B : weirdFourWord[wordLoc] = 'b'; break;
-
 			case C : weirdFourWord[wordLoc] = 'c'; break;
-
 			case D : weirdFourWord[wordLoc] = 'd'; break;
 
 		}
-
-		wordLoc++;
-
+		wordLoc--;
 	}
-
+	wordLoc = (WORDLENGTH/2);
 	weirdFourWord[wordLoc] = 0;
-
 	return weirdFourWord;
-
 }
 
-
-
-void addToErrorList(Errorptr *head, Errorptr *last, errorType err, int numLine){
-
+void addToErrorList(Errorptr *head, Errorptr *last, errorType err, int numLine)
+{
 	Errorptr temp,p;
-
-	temp = (Errorptr) malloc(sizeof(error_node));/* allocate memory for a new 														Error in list*/
-
-	if (!temp){
-
+	temp = (Errorptr) malloc(sizeof(error_node));/* allocate memory for a new Error in list*/
+	if (!temp)
+	{
 		fprintf(stderr, "Memory allocation failed\n");
-
 		exit(0);
-
 	}
-
 	/*new item is put in a temporary node before entering the list*/
-
 	temp->title = err;
-
 	temp->lineNumber = numLine;
-
 	temp->next = NULL;
-
-	if (!(*head)){	/*insert to an empty list*/
-
+	if (!(*head))
+	{	
+		/*insert to an empty list*/
 		*last = temp;
-
 		*head = temp;
-
 	}
-
-	else {	/*insert to a non-empty list */
-
+	else 
+	{	
+		/*insert to a non-empty list */
 		p = *last;
-
 		p->next = temp;
-
 		*last = p->next;
 
 	}		
-
 }
 
 /*gets a string version of enum value*/
-
-const char *getError(errorType e){
-
-	switch (e){
-
+const char *getError(errorType e)
+{
+	switch (e)
+	{
 		case NONE: return "NONE";
-
 		case WRONG_INPUT_IN_LABEL : return "WRONG_INPUT_IN_LABEL";
-		
 		case SYMBOL_ALREADY_IN_THE_TABLE : return "SYMBOL_ALREADY_IN_THE_TABLE"; 
-
 		case LABEL_INPUT_TOO_LONG : return "LABEL_INPUT_TOO_LONG";		
-		
+		case WRONG_INPUT_VALUE : return "WRONG_INPUT_VALUE";
+		case LABEL_INPUT_NOT_EXSIST: return "LABEL_INPUT_NOT_EXSIST";
 		case WRONG_INSTRUCTION_NAME : return "WRONG_INSTRUCTION_NAME";
-
 		case WRONG_PARAMETER_VALUE : return "WRONG_PARAMETER_VALUE";
-
 		case WRONG_MATRIX_SIZE : return "WRONG_MATRIX_SIZE";
-
 		case NO_LABEL_FOR_MATRIX : return "NO_LABEL_FOR_MATRIX";
-
 		case WRONG_MATRIX_PARAMETER_VALUE : return "WRONG_MATRIX_PARAMETER_VALUE";				
-	
 		case WRONG_OPERAND : return "WRONG_OPERAND";
-
 		case TOO_MANY_OPERANDS : return "TOO_MANY_OPERANDS";
-
 		case NO_LABEL_FOR_EXTERN_INSTRUCTION : return "NO_LABEL_FOR_EXTERN_INSTRUCTION";
-
 		case ERROR_IN_LABEL_FOR_EXTERN_INSTRUCTION : return "ERROR_IN_LABEL_FOR_EXTERN_INSTRUCTION";
-
 		default: return NULL;	
-
 	}
-
 }
 
 /*prints error trace stack*/ 
-
-void printErrorList(Errorptr head){
+void printErrorList(Errorptr head)
+{
 
 	Errorptr p;
-
 	p = head;
-
-	while(p != NULL){
+	while(p != NULL)
+	{
 		printf("%s, in line: %d\n",getError(p->title), p->lineNumber);				
-
 		p=p->next;
-
 	}
-
 	printf("\n");
-
 	return;	
-
 }
 
-
-
-char *isOpCode(char *word){
+char *isOpCode(char *word)
+{
 
 	int i;
 
-	for (i=0;i<16;i++){
-
-		if(strcmp(word,opcodesArray[i].opcodeName)==0){
+	for (i=0;i<16;i++)
+	{
+		if(strcmp(word,opcodesArray[i].opcodeName)==0)
+		{
 
 			return word;		
 
 		}	
-
 	}
-
 	return NULL;
-
 }
 
-void addToSymbolList(pSymbol *head,pSymbol *last, char *str, int counter,unsigned int isAct, unsigned int isExt)  {
-
+void addToSymbolList(pSymbol *head,pSymbol *last, char *str, int counter,unsigned int isAct, unsigned int isExt)  
+{
 	pSymbol temp,p;
-
-	temp = (pSymbol) malloc(sizeof(symbol));/* allocate memory for a new 														Error in list*/
-
-	if (!temp){
-
+	temp = (pSymbol) malloc(sizeof(symbol));/* allocate memory for a new Error in list*/
+	if (!temp)
+	{
 		fprintf(stderr, "Memory allocation failed\n");
-
 		exit(0);
-
 	}
-
 	/*new item is put in a temporary node before entering the list*/
-
 	temp->label = str;
-
 	temp->address = counter;
-
 	temp->action = isAct;
-
 	temp->external = isExt;
-
 	temp->next = NULL;
-
-	if (!(*head)){	/*insert to an empty list*/
-
+	printf("Label: %s, Address: %d, External: %d, Action: %d\n",temp->label, temp->address, temp->external, temp->action);
+	if (!(*head))
+	{	
+		/*insert to an empty list*/
 		*last = temp;
-
 		*head = temp;
-
 	}
-
-	else {	/*insert to a non-empty list */
-
+	else 
+	{	
+		/*insert to a non-empty list */
 		p = *last;
-
 		p->next = temp;
-
 		*last = p->next;
-
 	}		
 
 }
@@ -243,21 +163,44 @@ pSymbol findSymbolByLabel(char *label)
 	return NULL;
 }
 
-void printSymbolList(pSymbol head){
-
+void printSymbolList(pSymbol head)
+{
 	pSymbol p;
-
 	p = head;
-
-	while(p != NULL){
+	while(p != NULL)
+	{
 		printf("Label: %s, Address: %d, External: %d, Action: %d\n",p->label, p->address, p->external, p->action);				
-
 		p=p->next;
-
 	}
-
 	printf("\n");
-
 	return;	
-
 }
+
+unsigned int getNumOfParamsOfOpCode(char *word)
+{
+	int i;
+	for (i=0;i<16;i++)
+	{
+
+		if(strcmp(word,opcodesArray[i].opcodeName)==0)
+		{
+			return opcodesArray[i].numOfParams;		
+		}	
+	}
+	return (unsigned int)NULL;
+}
+
+unsigned int getNumOfOpCode(char *word)
+{
+	int i;
+	for (i=0;i<16;i++)
+	{
+
+		if(strcmp(word,opcodesArray[i].opcodeName)==0)
+		{
+			return opcodesArray[i].opcodeNum;		
+		}	
+	}
+	return (unsigned int)NULL;
+}
+
