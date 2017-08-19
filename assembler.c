d1413ff5a707c15062d0fc5f82b1b045420376dc
riron22 <riron22@walla.co.il>
2017-08-19 14:12:55 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -2,24 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include "assembler.h"
+#include "SharedHeaderFile.h"
+#include "FirstTransitionHeader.h"
+#include "SecondTransitionHeader.h"
 
 void validateFileName(char *token);
 FILE *handleFileName(char *filename);
-int main(int argc, char *argv[]){
+
+int main(int argc, char *argv[])
+{
 	FILE *fd;	
 	char *suffix;
 	char *fileName;
 	int argumentIndex = 1;
-	/*First, check for command-line argument error. If An error is found, 			program stops immediately*/	
-	if (argc<2){
+	/*First, check for command-line argument error. If An error is found, program stops immediately*/	
+	if (argc<2)
+	{
 		printf("No File Reference in Command-Line Input\n");
 		exit(0);
 	}
-	printf("line 19\n");
-	while (argumentIndex<argc){
-		printf("%d\n", argumentIndex);		
+	while (argumentIndex<argc)
+	{	
 		fileName = (char*) malloc(sizeof(char)*maxFileName);
-		if (!fileName){
+		if (!fileName)
+		{
 			fprintf(stderr, "Memory allocation for file name failed\n");
 			exit(0);
 		}
@@ -28,7 +34,10 @@ int main(int argc, char *argv[]){
 		suffix = strtok(NULL,".");
 		validateFileName(suffix);		
 		fd = handleFileName(fileName);
-		/*fileFirstTransition(fd);*/
+		moveOverFileOne(fd);
+		printErrorList(errorList);
+		printSymbolList(SymbolTable);
+		/*printf("%s" , fileName);*/
 		printf("Let's Start!\n"); /*just for debugging*/
 		argumentIndex++;
 	}
@@ -36,20 +45,24 @@ int main(int argc, char *argv[]){
 	return 0;	
 }
 
-void validateFileName(char *token){
-	if ((token == NULL) || (strcmp(token,inputFileSuffix)!=0)){
+void validateFileName(char *token)
+{
+	if ((token == NULL) || (strcmp(token,inputFileSuffix)!=0))
+	{
 		fprintf(stderr, "Wrong File Type as Argument\n");
 		exit(0);
 	}
 }
 
-FILE *handleFileName(char *filename){
+FILE *handleFileName(char *filename)
+{
 	FILE *f;	
 	if (!(f = fopen(filename,"r")))
 	{
 		fprintf(stderr,"cannot open file\n");
 		exit(0);
-	} else {
+	} else 
+	{
 		return f;
 	}
 }
